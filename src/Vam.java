8df939fe5cd75a02f3892e46ed5b5ae9b914ffb2
SBester001 <38667525+SBester001@users.noreply.github.com>
2019-01-21 19:33:27 +0100

    Initial commit

---
+++ src/Vam.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+import javax.swing.*;
+
+public class Vam extends JFrame{
+    
+   
+    List lines = new ArrayList<JTextField>();
+    
+    Vam(){
+        setSize(800, 800);
+        setResizable(false);
+        setTitle("Virtual Assembling Machine");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        setVisible(true);
+        
+        for(int i = 0; i<10; i++) {
+            addLine(i+1);
+        }
+        
+    }
+    
+    public void addLine(int number) {
+        JPanel panel = new JPanel();
+        
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        JLabel label = new JLabel(String.valueOf(number));
+        lines.add(new JTextField());
+        ((JTextField)lines.get(number-1)).setSize(680, 20);
+        panel.add(label);
+        panel.add(((JTextField)lines.get(number-1)));
+        panel.setSize(360, 24);
+        panel.setVisible(true);
+        add(panel);
+    }
+    
+    public static void main(String args[]) {
+        new Vam();
+    }
+}
+
