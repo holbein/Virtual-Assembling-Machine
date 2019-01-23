32d3f1ecd82f6276c78d0430e64de8269d2419e3
victorOle <45484220+victorOle@users.noreply.github.com>
2019-01-23 15:23:19 +0100

    Update Vam.java

--- src/Vam.java
+++ src/Vam.java
@@ -1,9 +1,9 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 
 public class Vam extends JFrame{
@@ -52,7 +52,7 @@ public class Vam extends JFrame{
 		setTitle("Virtual Assembler Machine");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		textArea.addKeyListener(new MyKeyListener());
+		textArea.getDocument().addDocumentListener(new MyDocumentListener());
 		scrollPane.getVerticalScrollBar().setUnitIncrement(10); //sets the scroll-speed
 		
 		lineNumbering.setLayout(new GridLayout(numberOfLines, 2));
@@ -75,13 +75,12 @@ public class Vam extends JFrame{
 		setVisible(true);
 	}
 	
-	public class MyKeyListener implements KeyListener{
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				System.out.println("ENTER " + textArea.getLineCount()+" "+numberOfLines);
-				//System.out.println(textArea.getText());
-				numberOfLines = textArea.getLineCount()+1;
+    class MyDocumentListener implements DocumentListener {
+        final String newline = "\n";
+ 
+        public void insertUpdate(DocumentEvent e) {
+            if(textArea.getLineCount() > numberOfLines) {
+				numberOfLines++;
 				lineNumbering.setLayout(new GridLayout(numberOfLines, 2));
 				if(numberOfLines<10) {
 					lineNumbering.add(new JLabel(String.valueOf(numberOfLines)+"   "));
@@ -90,39 +89,21 @@ public class Vam extends JFrame{
 				}
 				lineNumbering.add(new JLabel(":"));
 			}
-			
-			if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-				System.out.println("BACKSPACE " + textArea.getLineCount()+" "+numberOfLines);
-				//System.out.println(textArea.getText());
-				if(textArea.getLineCount() == numberOfLines) {
-					try {
-						Robot r = new Robot();
-						System.out.println("BACVCK");
-						r.keyPress(KeyEvent.VK_BACK_SPACE);
-						r.keyRelease(KeyEvent.VK_BACK_SPACE);
-						System.out.println(textArea.getLineCount()+" "+numberOfLines);
-						if(textArea.getLineCount() != numberOfLines) {
-							numberOfLines = textArea.getLineCount()-1;
-							lineNumbering.setLayout(new GridLayout(numberOfLines, 2));
-							lineNumbering.remove(lineNumbering.getComponentCount()-1);
-							lineNumbering.remove(lineNumbering.getComponentCount()-1);
-						}
-					}catch(Exception ex) {}
-				}
+        }
+        
+        public void removeUpdate(DocumentEvent e) {
+            if(textArea.getLineCount() < numberOfLines) {
+				numberOfLines--;
+				lineNumbering.setLayout(new GridLayout(numberOfLines, 2));
+				lineNumbering.remove(lineNumbering.getComponentCount()-1);
+				lineNumbering.remove(lineNumbering.getComponentCount()-1);
 			}
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {}
-
-		@Override
-		public void keyTyped(KeyEvent e) {}
-	}
-	
-	public String getFullText() {
-		return textArea.getText();
-	}
-
+        }
+        public void changedUpdate(DocumentEvent e) {
+            //Plain text components don't fire these events.
+        }
+    }
+    
 	//line is the same number as the numbering of the lines on the right side 
 	public String getTextInLine(int line) {
 		String ret = textArea.getText();
@@ -201,74 +182,77 @@ public class Vam extends JFrame{
 	
 	private void start() {
 		while(!stop) {
-			check(getTextInLine(BZ));
+			check(getTextInLine(BZ).trim());
 		}
 		byte[] by = {SR, BZ, A, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15};
 		panelRight = rightPanel(by);
 	}
 	
-	private void def(String input) {//dafault
+	private void def(String input) {//default
+		/*JFrame frame = new JFrame("Error");
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		frame.setSize(200, 100);
+		
+		JLabel lError = new JLabel("Unknown command: \"" + input +"\"!");
+		lError.setForeground(Color.RED);
+		
+		frame.add(lError);
+		frame.setVisible(true);*/        //Use only when there are no loop-bugs!!!!
 		System.out.println("unknown command: \"" + input +"\"");
+		BZ++;
 	}
 	
 	//calls the right method 
-	private void execute(String command, String rest) {
-		if(rest.length()>0) {
-			if(rest.charAt(0) == ' ') {
-				switch(command) {
-				case "ADD":
-					add(Integer.valueOf(rest.substring(1)));
-					break;
-				case "DLOAD":
-					dload(Integer.valueOf(rest.substring(1)));
-					break;
-				case "DIV":
-					div(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JEQ":
-					jeq(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JGE":
-					jge(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JGT":
-					jgt(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JLE":
-					jle(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JLT":
-					jlt(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JNE":
-					jne(Integer.valueOf(rest.substring(1)));
-					break;
-				case "JUMP":
-					jump(Integer.valueOf(rest.substring(1)));
-					break;
-				case "LOAD":
-					load(Integer.valueOf(rest.substring(1)));
-					break;
-				case "MULT":
-					mult(Integer.valueOf(rest.substring(1)));
-					break;
-				case "STORE":
-					store(Integer.valueOf(rest.substring(1)));
-					break;
-				case "SUB":
-					sub(Integer.valueOf(rest.substring(1)));
-					break;
-				}
-			}else {
-				def(command+rest);
-			}
-			
-		}else {
-			if(command.equals("END")) {
-				end();
-			}else {
-				def(command+rest);
-			}
+	private void execute(String command, int rest) {
+		switch(command) {
+		case "ADD":
+			add(rest);
+			break;
+		case "DLOAD":
+			dload(rest);
+			break;
+		case "DIV":
+			div(rest);
+			break;
+		case "JEQ":
+			jeq(rest);
+			break;
+		case "JGE":
+			jge(rest);
+			break;
+		case "JGT":
+			jgt(rest);
+			break;
+		case "JLE":
+			jle(rest);
+			break;
+		case "JLT":
+			jlt(rest);
+			break;
+		case "JNE":
+			jne(rest);
+			break;
+		case "JUMP":
+			jump(rest);
+			break;
+		case "LOAD":
+			load(rest);
+			break;
+		case "MULT":
+			mult(rest);
+			break;
+		case "STORE":
+			store(rest);
+			break;
+		case "SUB":
+			sub(rest);
+			break;
+		case "END":
+			end();
+			break;
+		default:
+			def(command+" "+rest);
+			break;
 		}
 	}
 	
@@ -278,46 +262,44 @@ public class Vam extends JFrame{
 	}
 	
 	private void add(int number) {
-		int i;
-		int temp;
+		int i = 0;
 		switch(number) {
-			case 1: i = R1;
-			case 2: i = R2;
-			case 3: i = R3;
-			case 4: i = R4;
-			case 5: i = R5;
-			case 6: i = R6;
-			case 7: i = R7;
-			case 8: i = R8;
-			case 9: i = R9;
-			case 10: i = R10;
-			case 11: i = R11;
-			case 12: i = R12;
-			case 13: i = R13;
-			case 14: i = R14;
-			case 15: i = R15;
+			case 1: i = R1; break;
+			case 2: i = R2; break;
+			case 3: i = R3; break;
+			case 4: i = R4; break;
+			case 5: i = R5; break;
+			case 6: i = R6; break;
+			case 7: i = R7; break;
+			case 8: i = R8; break;
+			case 9: i = R9; break;
+			case 10: i = R10; break;
+			case 11: i = R11; break;
+			case 12: i = R12; break;
+			case 13: i = R13; break;
+			case 14: i = R14; break;
+			case 15: i = R15; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
-				i=0;
 				end();
 		}
-		temp=A+i;
+		int temp=A+i;
 		
-		SR = (byte) (SR & Byte.valueOf("11111100")); //set last 2 bit to 0
+		SR = (byte) (SR & ~0x03); // clear last 2 bits
 		if(temp>0) {
-			SR = (byte) (SR | Byte.valueOf("00000010")); //set bit before last to 1
+			SR = (byte) (SR | 0x02); //set bit before last to 1
 		}else {
 			if(temp<0) {
-				SR = (byte) (SR | Byte.valueOf("00000001"));//set last bit to 1
+				SR = (byte) (SR | 0x01);//set last bit to 1
 			}
 		}
 		if(temp<128&&temp>-129) {
 			A=(byte)temp;
-			SR = (byte) (SR & Byte.valueOf("11111011"));//set 6. bit to 0
+			SR = (byte) (SR & ~0x04);// clear set 6. bit to 0
 										//           11111111112222222222333
 	}else {								//  12345678901234567890123456789012
-			A = (byte)(temp & Byte.valueOf("00000000000000000000000011111111"));
-			SR = (byte) (SR | Byte.valueOf("00000100"));//set 6. bit to 1
+			A = (byte)(temp & 0xFF);
+			SR = (byte) (SR | 0x04);//set 6. bit to 1
 		}
 		
 		BZ++;
@@ -333,30 +315,29 @@ public class Vam extends JFrame{
 	}
 	
 	private void div(int number) {
-		int i;
-		int temp;
+		int i = -1;
 		switch(number) {
-			case 1: i = R1;
-			case 2: i = R2;
-			case 3: i = R3;
-			case 4: i = R4;
-			case 5: i = R5;
-			case 6: i = R6;
-			case 7: i = R7;
-			case 8: i = R8;
-			case 9: i = R9;
-			case 10: i = R10;
-			case 11: i = R11;
-			case 12: i = R12;
-			case 13: i = R13;
-			case 14: i = R14;
-			case 15: i = R15;
+			case 1: i = R1; break;
+			case 2: i = R2; break;
+			case 3: i = R3; break;
+			case 4: i = R4; break;
+			case 5: i = R5; break;
+			case 6: i = R6; break;
+			case 7: i = R7; break;
+			case 8: i = R8; break;
+			case 9: i = R9; break;
+			case 10: i = R10; break;
+			case 11: i = R11; break;
+			case 12: i = R12; break;
+			case 13: i = R13; break;
+			case 14: i = R14; break;
+			case 15: i = R15; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
 				i=0;
 				end();
 		}
-		temp=A/i;
+		int temp=A/i;
 		
 		SR = (byte) (SR & Byte.valueOf("11111100")); //set last 2 bit to 0
 		if(temp>0) {
@@ -438,21 +419,21 @@ public class Vam extends JFrame{
 	private void load(int number) {
 		int i;
 		switch(number) {
-			case 1: i = R1;
-			case 2: i = R2;
-			case 3: i = R3;
-			case 4: i = R4;
-			case 5: i = R5;
-			case 6: i = R6;
-			case 7: i = R7;
-			case 8: i = R8;
-			case 9: i = R9;
-			case 10: i = R10;
-			case 11: i = R11;
-			case 12: i = R12;
-			case 13: i = R13;
-			case 14: i = R14;
-			case 15: i = R15;
+			case 1: i = R1; break;
+			case 2: i = R2; break;
+			case 3: i = R3; break;
+			case 4: i = R4; break;
+			case 5: i = R5; break;
+			case 6: i = R6; break;
+			case 7: i = R7; break;
+			case 8: i = R8; break;
+			case 9: i = R9; break;
+			case 10: i = R10; break;
+			case 11: i = R11; break;
+			case 12: i = R12; break;
+			case 13: i = R13; break;
+			case 14: i = R14; break;
+			case 15: i = R15; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
 				i=0;
@@ -475,21 +456,21 @@ public class Vam extends JFrame{
 		int i;
 		int temp;
 		switch(number) {
-			case 1: i = R1;
-			case 2: i = R2;
-			case 3: i = R3;
-			case 4: i = R4;
-			case 5: i = R5;
-			case 6: i = R6;
-			case 7: i = R7;
-			case 8: i = R8;
-			case 9: i = R9;
-			case 10: i = R10;
-			case 11: i = R11;
-			case 12: i = R12;
-			case 13: i = R13;
-			case 14: i = R14;
-			case 15: i = R15;
+			case 1: i = R1; break;
+			case 2: i = R2; break;
+			case 3: i = R3; break;
+			case 4: i = R4; break;
+			case 5: i = R5; break;
+			case 6: i = R6; break;
+			case 7: i = R7; break;
+			case 8: i = R8; break;
+			case 9: i = R9; break;
+			case 10: i = R10; break;
+			case 11: i = R11; break;
+			case 12: i = R12; break;
+			case 13: i = R13; break;
+			case 14: i = R14; break;
+			case 15: i = R15; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
 				i=0;
@@ -518,21 +499,21 @@ public class Vam extends JFrame{
 	
 	private void store(int number) {
 		switch(number) {
-			case 1: R1 = A;
-			case 2: R2 = A;
-			case 3: R3 = A;
-			case 4: R4 = A;
-			case 5: R5 = A;
-			case 6: R6 = A;
-			case 7: R7 = A;
-			case 8: R8 = A;
-			case 9: R9 = A;
-			case 10: R10 = A;
-			case 11: R11 = A;
-			case 12: R12 = A;
-			case 13: R13 = A;
-			case 14: R14 = A;
-			case 15: R15 = A;
+			case 1: R1 = A; break;
+			case 2: R2 = A; break;
+			case 3: R3 = A; break;
+			case 4: R4 = A; break;
+			case 5: R5 = A; break;
+			case 6: R6 = A; break;
+			case 7: R7 = A; break;
+			case 8: R8 = A; break;
+			case 9: R9 = A; break;
+			case 10: R10 = A; break;
+			case 11: R11 = A; break;
+			case 12: R12 = A; break;
+			case 13: R13 = A; break;
+			case 14: R14 = A; break;
+			case 15: R15 = A; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
 				end();
@@ -543,21 +524,21 @@ public class Vam extends JFrame{
 		int i;
 		int temp;
 		switch(number) {
-			case 1: i = R1;
-			case 2: i = R2;
-			case 3: i = R3;
-			case 4: i = R4;
-			case 5: i = R5;
-			case 6: i = R6;
-			case 7: i = R7;
-			case 8: i = R8;
-			case 9: i = R9;
-			case 10: i = R10;
-			case 11: i = R11;
-			case 12: i = R12;
-			case 13: i = R13;
-			case 14: i = R14;
-			case 15: i = R15;
+			case 1: i = R1; break;
+			case 2: i = R2; break;
+			case 3: i = R3; break;
+			case 4: i = R4; break;
+			case 5: i = R5; break;
+			case 6: i = R6; break;
+			case 7: i = R7; break;
+			case 8: i = R8; break;
+			case 9: i = R9; break;
+			case 10: i = R10; break;
+			case 11: i = R11; break;
+			case 12: i = R12; break;
+			case 13: i = R13; break;
+			case 14: i = R14; break;
+			case 15: i = R15; break;
 			default: 
 				System.out.println(number+"is not a valid register!");
 				i=0;
@@ -586,6 +567,63 @@ public class Vam extends JFrame{
 	
 	//separates the command and the rest
 	public void check(String input) {
+		
+		if (input.equals("END")) {
+			execute("END", 0);
+			return;
+		}
+		
+		int space = input.indexOf(' ');			
+		if (space == -1) { // rubbish
+			def(input);
+			return;
+		}
+		
+		String com = input.substring(0, space);
+		
+		switch(com) {
+			case "LOAD": case "STORE": case "ADD": case "SUB": case "MULT": case "DIV":{
+				try {
+					int number = Integer.parseInt(input.substring(space+1));
+					if(0 < number && number < 0) {
+						execute(com, number);
+					}else def(input);
+				}catch(Exception e) {
+					def(input);
+				}
+			}
+			break;
+			
+			case "DLOAD":{
+				try {
+					int number = Integer.parseInt(input.substring(space+1));
+					if(-129 < number && number < 128) {
+						execute(com, number);
+					}else def(input);
+				}catch(Exception e) {
+					def(input);
+				}
+			}
+			break;
+			
+			case "JUMP": case "JGE": case "JGT": case "JLE": case "JLT": case "JEQ": case "JNE":{
+				try {
+					int number = Integer.parseInt(input.substring(space+1));
+					if(0 < number && number <= textArea.getLineCount()) {
+						execute(com, number);
+					}
+					else {
+						def(input);
+					}
+				}catch(Exception e) {
+					def(input);
+				}
+			}
+			break;
+			
+			default: def(input);
+		}
+		/*
 		switch(input.charAt(0)){
 		case 'A'://A
 			switch(input.charAt(1)){
@@ -784,7 +822,7 @@ public class Vam extends JFrame{
 		
 		default:
 			def(input);
-		}
+		}*/
 	}
 	
 	public static void main(String args[]) {
