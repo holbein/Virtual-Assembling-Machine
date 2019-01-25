a418a59ae7baf2cdf3ee4a99372eb1cebd2eba2c
SBester001 <38667525+SBester001@users.noreply.github.com>
2019-01-25 17:03:01 +0100

    new shortcuts

--- src/Vam.java
+++ src/Vam.java
@@ -1,9 +1,13 @@
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.MenuShortcut;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.*;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -126,7 +131,7 @@ public class Vam extends JFrame{
 		}*/
 		
 	}
-
+	
 	private void setMenu() {
 		bar = new JMenuBar();
 		menu = new JMenu("File");
@@ -155,6 +160,9 @@ public class Vam extends JFrame{
 				open();
 			}
 		});
+		
+		save.setAccelerator(KeyStroke.getKeyStroke('S', Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask())); //shortcut ctrl+s
+		open.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask())); //shortcut ctrl+o
 
 		menu.add(save);
 		menu.add(saveAs);
