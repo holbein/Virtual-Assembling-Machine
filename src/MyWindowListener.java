3f48a9af85565fcf13d3356d072ad1bf4c8a8794
SBester001 <38667525+SBester001@users.noreply.github.com>
2019-01-28 16:59:03 +0100

    flashing icon when iconified or deactivated

--- src/MyWindowListener.java
+++ src/MyWindowListener.java
@@ -1,16 +1,38 @@
+import java.awt.Image;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 
 public class MyWindowListener implements WindowListener{
 	Vam parent;
+	boolean iconified;
+	
+	private static final int sleepTime = 150;
+	
+	List <Image> red = new ArrayList<Image>();
+	List <Image> yellow = new ArrayList<Image>();
 	
 	public MyWindowListener(Vam parent) {
 		this.parent = parent;
+		
+		red.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/red_128x128.png")).getImage());
+		red.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/red_64x64.png")).getImage());
+		red.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/red_32x32.png")).getImage());
+		red.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/red_16x16.png")).getImage());
+		
+		yellow.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/yellow_128x128.png")).getImage());
+		yellow.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/yellow_64x64.png")).getImage());
+		yellow.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/yellow_32x32.png")).getImage());
+		yellow.add(new ImageIcon(Vam.class.getResource("resources/notReallyNecessary/yellow_16x16.png")).getImage());
 	}
 	
 	@Override
 	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
+		iconified = false;
+		parent.setIconImages(parent.holbeinLogos);
 		
 	}
 
@@ -28,26 +50,48 @@ public class MyWindowListener implements WindowListener{
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
+		iconified = true;
+		new coloreChanger(parent).start();
 		
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
+		iconified = false;
+		parent.setIconImages(parent.holbeinLogos);
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
+		iconified = true;
+		new coloreChanger(parent).start();
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	class coloreChanger extends Thread {
+		Vam parent;
+		public coloreChanger(Vam parent) {
+			this.parent = parent;
+		}
+		
+		@Override
+		public void run() {
+			long i = 0;
+			while(iconified) {
+				if(i%2 == 0) {
+					parent.setIconImages(red);
+				}else {
+					parent.setIconImages(yellow);
+				}
+				i++;
+				try {sleep(sleepTime);} catch (InterruptedException e1) {e1.printStackTrace();}
+			}
+		}
 	}
 
 }
